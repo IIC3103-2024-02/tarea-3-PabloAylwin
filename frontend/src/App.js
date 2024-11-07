b8066189939b71a8f853199e60c2de3f5c005983
Pablo Aylwin <p.aylwin.k@uc.cl>
2024-11-07 15:25:36 -0300

    visual fix and prompt edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Tarantibot</title>
+      </Helmet>
       <Main />
     </div>
   );
